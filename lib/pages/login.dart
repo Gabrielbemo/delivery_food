289fec6d92c19eec72751321868894256c814d27
Gabrielbemo <gabrielbemoura@gmail.com>
2022-03-24 22:51:49 -0300

    login page wip

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,14 +3,74 @@ import 'package:flutter/material.dart';
 //import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
-  const Login({ Key? key }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('asdasd'),
-      )
+      backgroundColor: Color.fromARGB(255,233,255,252),
+      body: Column(
+        children: [
+          Image(
+            image: NetworkImage(
+                'https://cdn.discordapp.com/attachments/956310286866972794/956722285975306260/Blue_Restaurant_Logo__1_-removebg-preview.png'),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+            child: const TextField(
+              decoration: InputDecoration(
+                fillColor: Color.fromARGB(255, 211, 6, 6),
+                isDense: true,
+                border: OutlineInputBorder(),
+                labelText: 'Usuario',
+              ),
+            ),
+          ),
+          
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
+            child: const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                isDense: true,
+                border: OutlineInputBorder(),
+                labelText: 'Senha',
+              ),
+            ),
+          ),
+
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF0D47A1),
+                          Color(0xFF1976D2),
+                          Color(0xFF42A5F5),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Login'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
