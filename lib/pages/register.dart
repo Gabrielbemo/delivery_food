e1145c2b15d0dd590c6018702baa17c3db8bed9c
Lucas Belinelli Pereira <lucas.pereira@ykt.com.br>
2022-03-27 21:56:13 -0300

    tela de registro completa, falta apenas validar os campos

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_food/pages/login.dart';
 import 'package:flutter/material.dart';
 
 //import 'package:flutter/material.dart';
@@ -9,13 +10,32 @@ class Register extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 233, 255, 252),
+      appBar: AppBar(),
       body: Column(
         children: [
-          Padding(padding: EdgeInsets.symmetric(vertical: 70,horizontal:2)),
-          Text(
-            'Seja Bem-Vindo!!'
-            'Informe seus dados pessoais.'
-          ),       
+          Padding(padding: EdgeInsets.symmetric(vertical: 30, horizontal: 2)),
+          Container(
+            padding: EdgeInsets.only(left: 25),
+            child: Text(
+              'Seja Bem-Vindo!!',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: Color.fromRGBO(24, 54, 105, 1)),
+            ),
+            alignment: Alignment.centerLeft,
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 25),
+            child: Text(
+              'Informe seus dados pessoais.',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Color.fromRGBO(24, 54, 105, 0.5)),
+            ),
+            alignment: Alignment.centerLeft,
+          ),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 12, horizontal: 25),
             child: const TextField(
@@ -60,37 +80,59 @@ class Register extends StatelessWidget {
               ),
             ),
           ),
-          MyStatefulWidget(),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Stack(
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 1, horizontal: 10),
+            child: Row(
               children: <Widget>[
-                Positioned.fill(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                        color: Color.fromRGBO(24, 54, 105, 1)),
+                MyStatefulWidget(),
+                Text(
+                  'Li e concordo',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
                   ),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    primary: Colors.white,
-                    minimumSize: Size(232, 56),
-                    textStyle: const TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                  },
-                  child: const Text('Enviar'),
-                ),
+                )
               ],
+            ),
+            alignment: Alignment.centerLeft,
+          ),
+          Container(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(24, 54, 105, 1)),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      primary: Colors.white,
+                      minimumSize: Size(346, 56),
+                      textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Login()),
+                      );
+                    },
+                    child: const Text('Enviar'),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
-    );    
+    );
   }
-  
 }
+
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
