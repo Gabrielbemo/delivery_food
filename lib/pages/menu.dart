ee50e957582d6c87237e1d19293c162fc7509f23
Gabrielbemo <gabrielbemoura@gmail.com>
2022-04-01 18:27:36 -0300

    wip

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -33,12 +33,6 @@ class Menu extends StatelessWidget {
                   size: 26.0,
                 ),
               )),
-          Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                onTap: () {},
-                child: const Icon(Icons.more_vert),
-              )),
         ],
       ),
       body: Column(
