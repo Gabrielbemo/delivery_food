b3ef8684a5b995e25bc9120b4d21d5eef583f0ee
Gabrielbemo <gabrielbemoura@gmail.com>
2022-03-31 22:07:48 -0300

    wip detalhe food

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -2,10 +2,12 @@ class Food {
   int id;
   String name;
   double price;
+  String igredients;
 
   Food({
     required this.id,
     required this.name,
     required this.price,
+    required this.igredients,
   });
 }
--- lib/repositories/food_repository.dart
+++ lib/repositories/food_repository.dart
@@ -5,27 +5,32 @@ class FoodRepository {
     Food(
       id: 1,
       name: "X-Burger",
-      price: 10
+      price: 10,
+      igredients: "HAMBURGER, PAO, SAL"
     ),
     Food(
       id: 2,
       name: "X-Salada",
-      price: 12
+      price: 12,
+      igredients: "HAMBURGER, PAO, SAL"
     ),
     Food(
       id: 3,
       name: "X-Calabresa",
-      price: 13
+      price: 13,
+      igredients: "HAMBURGER, PAO, SAL"
     ),
     Food(
       id: 4,
       name: "X-Bacon",
-      price: 14
+      price: 14,
+      igredients: "HAMBURGER, PAO, SAL"
     ),
     Food(
       id: 5,
       name: "X-Frango",
-      price: 14
+      price: 14,
+      igredients: "HAMBURGER, PAO, SAL"
     )
   ];
 }
